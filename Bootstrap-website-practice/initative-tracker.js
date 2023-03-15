01132af1b3fd8c0188c1c2dcb3fec8d230e964d6
Richard Davidsson <r-1chard@hotmail.com>
2023-03-15 17:59:07 +0100

    progress

--- Bootstrap-website-practice/initative-tracker.js
+++ Bootstrap-website-practice/initative-tracker.js
@@ -1,5 +1,4 @@
 function selected(event, item) {
-    let itemToInsertBefore = document.getElementById('#clear');
     if(item.style.backgroundColor === "rgb(220, 53, 69)"){
         item.style.backgroundColor = "white"
         item.parentNode.append(item)
@@ -21,4 +20,26 @@ function expandConditionbar(){
         item.style.display = "grid";
     }
 }
+function selected1(event, item){
+    let x = document.querySelector(".popout-container");
+    console.log(item.parentNode.parentNode);
+}
+    /*
+    if(item.parentNode === x){
+        console.log("yes");
+    }
+    }
+
+
+
+/*
+characterbar container
+    character picture container
+    character container
+
+    condition bar
+
+    popout container
+
+*/
 
